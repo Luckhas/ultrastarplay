2e08fb1ab6f427da8e0858ebcef3508a5b001e7e
Lucas Ferreira <ldcf.lucas@outlook.com>
2020-09-03 16:48:43 -0300

    index pronta

---
+++ src/components/components/carouselItem/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+function TestItem() {
+    return (
+        <div className="carousel-item active">
+            <div className="testimonial-item row">
+                <div className="col-md-4">
+                    <div className="row">
+                        <div className="col-xl-6 photo">
+                            <img src="/assets/images/testimonial/1.jpg" alt="" />
+                        </div>
+                        <div className="col-xl-6">
+                            <div className="stars">
+                                <span className="golden-star">
+                                    <i className="fas fa-star"></i>
+                                </span>
+                                <span className="golden-star">
+                                    <i className="fas fa-star"></i>
+                                </span>
+                                <span className="golden-star">
+                                    <i className="fas fa-star"></i>
+                                </span>
+                                <span className="golden-star">
+                                    <i className="fas fa-star"></i>
+                                </span>
+                                <span className="">
+                                    <i className="fas fa-star"></i>
+                                </span>
+                            </div>
+                            <div className="information">
+                                <p>Marcelo Henrique</p>
+                                <p>2 meses atrás</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md">
+                    <i className="fas fa-quote-left quoteL"></i>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. In voluptatem accusantium omnis, nam, quidem, quo voluptatum ducimus nemo facilis cupiditate esse. Itaque at molestiae rerum, nisi exercitationem unde illo ratione numquam aperiam fuga a odio labore amet sunt quidem architecto, error non ab est laborum blanditiis
+                        doloremque.
+                    </p>
+
+                    <i className="fas fa-quote-right quoteR"></i>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TestItem;
---
+++ src/components/menu/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Menu() {
+    return (
+        <nav className="navbar navbar-expand-lg container">
+            <a className="navbar-brand" href="#">
+                <img src="/assets/images/logo.png"/>
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                {/* <span className="navbar-toggler-icon">
+                    <i className="fas fa-bars"></i>
+                </span> */}
+                <i className="fas fa-bars navbar-toggler-icon"></i>
+
+            </button>
+
+            <div className="collapse navbar-collapse" id="mainNavbar">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Início <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Preço</a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Produtos</a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Download</a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Inscreva-se</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Menu;
